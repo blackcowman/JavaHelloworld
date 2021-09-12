404fdc243c3812b88828e00294d7bfc54b7a421b
blackcowman <soh990301@gmail.com>
2021-09-12 20:03:58 +0900

    HAHAH

--- Hello/com/example/lab1/Main.java
+++ Hello/com/example/lab1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
 		System.out.print("이름입력 : ");
-		String name = s.next();
-		System.out.println("[" + name + "]님 환영합니다!!~!~!!!!!" );
+		String name = s.nextLine();
+		System.out.println(name + "님 사랑합니다 ~~!!");
 	}
 }
---
+++ Hello/com/example/lab2/ArrayList.java
@@ -0,0 +1,12 @@
+package com.example.lab2;
+
+public class ArrayList {
+	public class Student{
+		int no;
+		String name;
+		int age;
+		
+		
+		
+	}
+}
